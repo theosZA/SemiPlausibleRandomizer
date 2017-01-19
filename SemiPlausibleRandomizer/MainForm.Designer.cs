9471e3a43ca61cbe6d9ea9cab50cc6a50500a311
Unknown <theosZA+github@googlemail.com>
2017-01-19 22:21:26 +0200

    Initial project reads regions from EU4 file

---
+++ SemiPlausibleRandomizer/MainForm.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace SemiPlausibleRandomizer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.RegionList = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // RegionList
+            // 
+            this.RegionList.FormattingEnabled = true;
+            this.RegionList.Location = new System.Drawing.Point(12, 38);
+            this.RegionList.Name = "RegionList";
+            this.RegionList.Size = new System.Drawing.Size(175, 304);
+            this.RegionList.TabIndex = 0;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 480);
+            this.Controls.Add(this.RegionList);
+            this.Name = "MainForm";
+            this.Text = "Semi-plausible Randomizer";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckedListBox RegionList;
+    }
+}
+
---
+++ SemiPlausibleRandomizer/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using Pfarah;
+using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace SemiPlausibleRandomizer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var dialog = new FolderBrowserDialog()
+            {
+                Description = "Select the Europa Universalis IV folder",
+                SelectedPath = @"C:\Program Files (x86)\Steam\steamapps\common\Europa Universalis IV\"
+            };
+            if (dialog.ShowDialog(this) == DialogResult.OK)
+            {
+                var path = dialog.SelectedPath;
+
+                // Load regions
+                var regions = ParaValue.LoadText(path + @"\map\region.txt") as ParaValue.Record;
+                var regionNames = regions.properties.Select(x => x.Item1).Skip(1);  // We skip the first region because it's special for the Random New World
+                RegionList.Items.AddRange(regionNames.ToArray());
+
+            }
+        }
+    }
+}
