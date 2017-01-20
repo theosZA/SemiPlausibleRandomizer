f5d32454da76ba54add73fa869726011934e1c8c
Unknown <theosZA+github@googlemail.com>
2017-01-20 19:37:14 +0200

    Mod class to hold all mod-specific content

---
+++ SemiPlausibleRandomizer/Mod/EU4Mod.cs
@@ -0,0 +1,89 @@
+﻿using SemiPlausibleRandomizer.EU4;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace SemiPlausibleRandomizer.Mod
+{
+    internal class EU4Mod
+    {
+        public string Name { get; set; }
+        public string EU4Version { get; set; }
+        public World EU4World { get; set; }
+
+        public void AddNewCountry(Province homeProvince)
+        {
+            var country = new Country()
+            {
+                Key = CountryIndexToTag(countries.Count),
+                GraphicalCulture = "westerngfx",
+                Color = GetNextRandomColor(),
+                Government = "feudal_monarchy",
+                GovernmentRank = 1,
+                PrimaryCulture = homeProvince.Culture,
+                Religion = homeProvince.Religion,
+                TechnologyGroup = "western",
+                CapitalProvinceKey = homeProvince.Key
+            };
+            countries.Add(country);
+            homeProvince.Owner = country.Key;
+            homeProvince.Controller = country.Key;
+            homeProvince.Cores = new string[] { country.Key };
+            provinces.Add(homeProvince);
+        }
+
+        public void Save(string modPath)
+        {
+            // Create the mod.
+            string[] modLines =
+            {
+                $"name = \"{Name}\"",
+                $"path = \"mod/{Name}\"",
+                $"supported_version = \"{EU4Version}\""
+            };
+            File.WriteAllLines($"{modPath}\\{Name}.mod", modLines);
+            string ourModPath = $"{modPath}\\{Name}";
+            Directory.CreateDirectory(ourModPath);
+            Directory.CreateDirectory($"{ourModPath}\\history");
+            Directory.CreateDirectory($"{ourModPath}\\history\\countries");
+            Directory.CreateDirectory($"{ourModPath}\\history\\provinces");
+            Directory.CreateDirectory($"{ourModPath}\\common");
+            Directory.CreateDirectory($"{ourModPath}\\common\\countries");
+            Directory.CreateDirectory($"{ourModPath}\\common\\country_tags");
+
+            // Create the country files.
+            foreach (var country in countries)
+            {
+                country.Save(ourModPath);
+            }
+            File.WriteAllLines($"{ourModPath}\\common\\country_tags\\randomized_contries.txt",
+                               countries.Select(country => $"{country.Key} = \"countries/{country.Key}.txt\"").ToArray());
+
+            // Create the province files.
+            foreach (var province in provinces)
+            {
+                province.Save($"{ourModPath}\\history\\provinces");
+            }
+        }
+
+        private string CountryIndexToTag(int index)
+        {
+            int value = index % 100;
+            const string prefixChars = "RSTUVWXYZQPONMLKJIHGFEDCBA";
+            char prefix = prefixChars[index / 100];
+            return $"{prefix}{value / 10}{value % 10}";
+        }
+
+        private Color GetNextRandomColor()
+        {
+            return Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+        }
+
+        Random random = new Random();
+
+        List<Country> countries = new List<Country>();
+        List<Province> provinces = new List<Province>();
+    }
+}
--- SemiPlausibleRandomizer/Randomizer.cs
+++ SemiPlausibleRandomizer/Randomizer.cs
@@ -1,9 +1,6 @@
 ﻿using SemiPlausibleRandomizer.EU4;
-using System;
+using SemiPlausibleRandomizer.Mod;
 using System.Collections.Generic;
-using System.Drawing;
-using System.IO;
-using System.Linq;
 
 namespace SemiPlausibleRandomizer
 {
@@ -23,74 +20,20 @@ namespace SemiPlausibleRandomizer
             var provinces = world.GetProvincesInRegions(regionNames);
 
             // For now we make one country per province.
-            var countries = new List<Country>();
-            foreach (var province in provinces)
+            var mod = new EU4Mod()
             {
-                var country = new Country()
-                {
-                    Key = CountryIndexToTag(countries.Count),
-                    GraphicalCulture = "westerngfx",
-                    Color = GetNextRandomColor(),
-                    Government = "feudal_monarchy",
-                    GovernmentRank = 1,
-                    PrimaryCulture = province.Culture,
-                    Religion = province.Religion,
-                    TechnologyGroup = "western",
-                    CapitalProvinceKey = province.Key
-                };
-                countries.Add(country);
-                province.Owner = country.Key;
-                province.Controller = country.Key;
-                province.Cores = new string[] { country.Key };
-            }
-
-            // Create the mod.
-            string modName = "Randomized World";
-            string[] modLines = 
-            {
-                $"name = \"{modName}\"",
-                $"path = \"mod/{modName}\"",
-                "supported_version = \"1.19\""
+                Name = "Randomized World",
+                EU4Version = "1.19",
+                EU4World = world
             };
-            File.WriteAllLines($"{modPath}\\{modName}.mod", modLines);
-            string ourModPath = $"{modPath}\\{modName}";
-            Directory.CreateDirectory(ourModPath);
-            Directory.CreateDirectory($"{ourModPath}\\history");
-            Directory.CreateDirectory($"{ourModPath}\\history\\countries");
-            Directory.CreateDirectory($"{ourModPath}\\history\\provinces");
-            Directory.CreateDirectory($"{ourModPath}\\common");
-            Directory.CreateDirectory($"{ourModPath}\\common\\countries");
-            Directory.CreateDirectory($"{ourModPath}\\common\\country_tags");
-
-            // Create the country files.
-            foreach (var country in countries)
-            {
-                country.Save(ourModPath);
-            }
-            File.WriteAllLines($"{ourModPath}\\common\\country_tags\\randomized_contries.txt",
-                               countries.Select(country => $"{country.Key} = \"countries/{country.Key}.txt\"").ToArray());
-
-            // Create the province files.
             foreach (var province in provinces)
             {
-                province.Save($"{ourModPath}\\history\\provinces");
+                mod.AddNewCountry(province);
             }
-        }
 
-        private string CountryIndexToTag(int index)
-        {
-            int value = index % 100;
-            const string prefixChars = "RSTUVWXYZQPONMLKJIHGFEDCBA";
-            char prefix = prefixChars[index / 100];
-            return $"{prefix}{value / 10}{value % 10}";
-        }
-
-        private Color GetNextRandomColor()
-        {
-            return Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+            mod.Save(modPath);
         }
 
         readonly World world;
-        Random random = new Random();
     }
 }
